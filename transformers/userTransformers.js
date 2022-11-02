36c2ab1ef2b7d1079c7ed0f8ce0ae42b9715b728
Amr241991 <amrsarori2004@gmail.com>
2022-11-02 11:16:18 +0300

    userTransformers

--- transformers/userTransformers.js
+++ transformers/userTransformers.js
@@ -1,17 +1,71 @@
-var userTransformer = function(user) {
-    if (user?.dataValues?.password) {
-        delete user?.dataValues?.password
+const userTransformer = (user) => {
+    const transformeredUser = {
+        name: '',
+        email:'',
+        avatar:'',
+        bgPic:'',
+        type:'', 
+        reviews: [],
     }
-    if (user?.Photos) {
-        user.Photos = photosTransformer(user.Photos)
+    if (user) {
+        transformeredUser.name = user?.username
+        transformeredUser.email = user?.email
+        transformeredUser.avatar = user?.UserInfo?.avatar
+        transformeredUser.bgPic = user?.UserInfo?.bgPic
+        transformeredUser.type = user?.UserType?.type
+        user?.Reviews?.map(review =>{
+            transformeredUser.reviews.push(review.content)
+        })
+        return transformeredUser
     }
     return user
 }
-var usersTransformer = function(members) {
-    return members.map(user => userTransformer(user))
+
+const userInfoTransformer = (userInfo) => {
+    console.log(userInfo)
+    if(userInfo){
+        delete userInfo['dataValues']['id']
+        delete userInfo['dataValues']['usersId']
+        delete userInfo['dataValues']['updatedAt']
+        delete userInfo['dataValues']['createdAt']
+        if(userInfo['dataValues']['usersId'])
+        delete userInfo['dataValues']['usersId']
+    }
+    return userInfo
+}
+
+const userTypeTransformer = (userType) => {
+    console.log(userType)
+    if(userType){
+        delete userType['dataValues']['id']
+        delete userType['dataValues']['updatedAt']
+        delete userType['dataValues']['createdAt']
+        if(userType['dataValues']['usersId'])
+        delete userType['dataValues']['usersId']
+    }
+    return userType
+}
+
+const reviewsTransformer = (reviews) => {
+    console.log(reviews)
+    const transformeredReviews = {
+        content: '',
+        User: {name: '', email:''}
+    }
+    if (reviews) {
+        transformeredReviews.content = reviews?.content
+        transformeredReviews.User.name = reviews?.User?.username
+        transformeredReviews.User.email = reviews?.User?.email
+
+        return transformeredReviews
+    }
+   
+    return reviews
 }
 
 module.exports = {
-    usersTransformer,
-    userTransformer
+    userTransformer,
+    userInfoTransformer,
+    userTypeTransformer,
+    reviewsTransformer
 }
